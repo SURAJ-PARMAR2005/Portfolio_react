5ef14bb4f2a82e73419355df82f0e34305c56d6c
SURAJ-PARMAR2005 <golurajput87080@gmail.comgit config --list>
2025-12-14 12:06:59 +0530

    Cursor Follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useRef } from 'react'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+
+
+gsap.registerPlugin(useGSAP);
+
 const App = () => {
+
+  const mouseRef = useRef(null);
+ useGSAP(() =>{
+
+ })
+  const follow = (dets) => {
+    gsap.to(mouseRef.current,{
+      x:dets.clientX,
+      y:dets.clientY,
+      duration:0.6,
+      // ease:"power3.out",
+    });
+  }
+
   return (
-    <div>
+    <div className='main' onMouseMove={follow} >
+      <div className='cursor' ref={mouseRef} ></div>
       <Navbar/>
       <Hero />
       <About />
